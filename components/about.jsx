cf4356a3f83a88d5d7088b34eba13876b22b758d
KerwinAngeles <kerwinangeles12@gmail.com>
2025-02-10 04:05:06 +0100

    Fix the paragraph

--- components/about.jsx
+++ components/about.jsx
@@ -7,8 +7,8 @@ export function About(){
             <img src="/yo.jpg" alt="" className='object-contain h-24 w-24 rounded-sm' />
         </div>
         <div className=''>
-        <p className="text-sm text-gray-200" style={{ textAlign: 'justify', lineHeight: '22px' }}>
-                Soy Kerwin Angeles Flores, un desarrollador <span style={{color: '#FACC15'}}> Frontend Developer </span> apasionado por la tecnología y la creación de soluciones innovadoras. 
+        <p className="text-sm text-gray-900 dark:text-gray-200" style={{ textAlign: 'justify', lineHeight: '22px' }}>
+                Soy Kerwin Angeles Flores, un desarrollador <span className='font-bold' style={{color: '#FACC15'}}> Frontend </span> apasionado por la tecnología y la creación de soluciones innovadoras. 
                 Tengo experiencia trabajando en una empresa de seguros, donde desarrollo nuevas funcionalidades para su sistema web, optimizando flujos de trabajo. 
                 Además, tengo experiencia trabajando con clientes para desarrollar sistemas web a medida. Actualmente, estoy estudiando en el Instituto Tecnológico de las Américas (ITLA), donde sigo ampliando mis conocimientos y habilidades en desarrollo de software.
             </p>
